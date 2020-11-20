557fa0e7375be429f7941c30778ae16b6e5243ec
Jonathan Paez <jfpazto@hotmail.com>
2020-11-20 17:52:31 -0500

    Cambiando algo en guice

--- src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
+++ src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -33,6 +33,6 @@ public class GuiceContextListener implements ServletContextListener {
             }
         });
 
-        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
+        //servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
     }
 }
